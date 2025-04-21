0a42ae1dde037f5b246fbd5225171eb11a26d9bb
Akintunde Teslim Akinmuyiwa <akinmuyiwateslim@gmail.com>
2025-04-21 11:15:45 +0100

    adding getAll, delete and  update logics

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -1,8 +1,8 @@
-const Employees = require("../models/Employees");
+const EmployeeDB = require("../models/Employees");
 
 const getAllEmployees = async (req, res) => {
   try {
-    const allEmployees = await Employees.find();
+    const allEmployees = await EmployeeDB.find();
     if (!allEmployees || allEmployees.length === 0) {
       return res.status(400).json({
         message: "There is no employee record. Click add to input records",
@@ -20,16 +20,59 @@ const createEmployee = async (req, res) => {
   if (!firstname || !lastname)
     return res.status(400).json({ message: "Missing required fields" });
   try {
-    const employeeExist = await Employees.findOne({ firstname });
+    const employeeExist = await EmployeeDB.findOne({ firstname });
     if (employeeExist) {
       return res.status(400).json({ message: "Employee already exist" });
     }
-    const newEmployee = await Employees.create({ firstname, lastname });
-    res.status(201).json({ message: "employee created successfully" });
+    await EmployeeDB.create({ firstname, lastname });
+    res.status(200).json({ message: "employee created successfully" });
   } catch (error) {
     console.log(error);
     return res.status(500).json(error);
   }
 };
 
-module.exports = { createEmployee, getAllEmployees };
+const updateEmployee = async (req, res) => {
+  if (!req?.body?.id)
+    return res.status(400).json({ message: "Missing required id" });
+
+  const employeeExist = await EmployeeDB.findOne({ _id: req.body.id });
+  if (!employeeExist)
+    return res
+      .status(404)
+      .json({ message: `Employee with this id ${req.body.id} not found` });
+  if (req.body.firstname) employeeExist.firstname = req.body.firstname;
+  if (req.body.lastname) employeeExist.lastname = req.body.lastname;
+  const result = await employeeExist.save();
+  res.status(200).json({ message: "employee updated successfully" });
+};
+
+const deleteEmployee = async (req, res) => {
+  const id = req.params.id;
+  const employeeExist = await EmployeeDB.findById(id);
+  if (!employeeExist)
+    return res
+      .status(404)
+      .json({ message: `Employee with this id ${id} not found` });
+  await EmployeeDB.findByIdAndDelete(id);
+  res.status(200).json({ message: "Employee deleted successfully" });
+};
+
+const getSingleEmployee = async (req, res) => {
+  const { id } = req.params;
+  if (!id) return res.status(400).json({ message: "No employee id present" });
+  const employee = await EmployeeDB.findOne({ _id: req.params.id }).exec();
+  if (!employee)
+    return res
+      .status(204)
+      .json({ message: `No employee matches ID ${req.params.id}` });
+  res.status(200).json(employee);
+};
+
+module.exports = {
+  createEmployee,
+  getAllEmployees,
+  updateEmployee,
+  deleteEmployee,
+  getSingleEmployee,
+};
--- server/routes/employee.js
+++ server/routes/employee.js
@@ -4,5 +4,8 @@ const controllers = require("../controllers/employeeController");
 
 router.post("/", controllers.createEmployee);
 router.get("/records", controllers.getAllEmployees);
+router.get("/record/:id", controllers.getSingleEmployee);
+router.put("/update/:id", controllers.updateEmployee);
+router.delete("/delete/:id", controllers.deleteEmployee);
 
 module.exports = router;
